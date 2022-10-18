d1cf100fef9028ae0dc0fd5b75369e10585b0ad0
Luca Marcellino <s234262@studenti.polito.it>
2022-10-18 23:39:23 +0200

    changeName

---
+++ lab1/README.md
@@ -0,0 +1,34 @@
+# Lab1
+In this repository is present a possibile solution for the set covering problem. I worked with my collegue Luca Villani (304992) in order to to find a best solution.
+The code print the sum of number of elements inside the lists (W). In the code the unit_cost is setted to the len of the list that the code will add, but in this case the computation time will increase exponentially when also the N increase. When we try to explore N=100 we can't find a good solution in a reasonable time.
+If we change the unit_cost to one we obtain the results here below. The results are not optiamal but the time to achive them is very good. we thought that this could be an optimal trade-off between (difference between optimal solution and found solution) and (computational time).
+
+
+Problem with N=5
+A* W=5; bloat=0%
+Done in 0.019455671310424805 seconds
+
+
+Problem with N=10
+A* W=12; bloat=20%
+Done in 0.0029790401458740234 seconds
+
+
+Problem with N=20
+A* W=32; bloat=60%
+Done in 0.002991199493408203 seconds
+
+
+Problem with N=100
+A* W=183; bloat=83%
+Done in 0.06474566459655762 seconds
+
+
+Problem with N=500
+A* W=1361; bloat=172%
+Done in 2.228945255279541 seconds
+
+
+Problem with N=1000
+A* W=2997; bloat=200%
+Done in 7.1089537143707275 seconds
---
+++ lab1/lab1/__init__.py
@@ -0,0 +1,149 @@
+import random
+from tkinter.tix import INTEGER
+import numpy as np
+from typing import Callable
+from gx_utils import *
+import logging
+import itertools
+import time
+
+def problem(N, seed=42):
+    random.seed(seed)
+    return [
+        list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
+        for n in range(random.randint(N, N * 5))
+    ]
+
+class State:
+    def __init__(self, data : np.ndarray):
+        self._data = data.copy()
+        self._data.flags.writeable = False
+
+    def __hash__(self):
+        return hash(bytes(self._data))
+
+    def __eq__(self, other):
+        return bytes(self._data) == bytes(other._data)
+
+    def __lt__(self, other):
+        return bytes(self._data) < bytes(other._data)
+
+    def __str__(self):
+        return str(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+    @property
+    def data(self):
+        return self._data
+
+    def copy_data(self):
+        return self._data.copy()
+
+def priority_function(state: State):
+     list_ = state.data.tolist()
+     state_cost_ = sum(len(s) for s in list_)
+     return state_cost_ + h(state)
+
+def possible_actions(all_list: list, actualState: list):
+    all_list1 = all_list.copy()
+    all_list1 = np.array(all_list1)
+    all_list1 = np.unique(all_list1).tolist()
+    for i in actualState:
+        all_list1.remove(i)
+    all_list1 = sorted(all_list1, key= lambda l: len(l))
+    return all_list1 
+    
+def goal_test(state,N):
+    set_ = set()
+    for el in state:
+        for a in el: 
+            set_.add(a)
+    set_goal = set(range(N))
+    return set_ == set_goal
+
+def result(state, a):
+    state.append(a)
+    return State(np.array(state,dtype=object))
+
+def h(state):
+    goal=set()
+    for list_ in state._data:
+        goal.update(list_)
+    return N-len(goal)
+
+
+
+def search(
+    all_list,
+    initial_state: State,
+    goal_test: Callable,
+    parent_state: dict,
+    state_cost: dict,
+    priority_function: Callable,
+    unit_cost: Callable,
+    number : INTEGER
+):
+    frontier = PriorityQueue()
+    parent_state.clear()
+    state_cost.clear()
+    state = initial_state
+    parent_state[state] = None
+    state_cost[state] = 0
+    #all_list.sort()
+
+    while state is not None and not goal_test(state.data.tolist(),N):
+        for a in possible_actions(all_list,state.data.tolist()):
+            new_state = result(state.data.tolist(), a)
+            cost = unit_cost(a)
+            if new_state not in state_cost and new_state not in frontier:
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                frontier.push(new_state, p=priority_function(new_state))
+                logging.debug(f"Added new node to frontier (cost={state_cost[new_state]})")
+            elif new_state in frontier and state_cost[new_state] > state_cost[state] + cost:
+                old_cost = state_cost[new_state]
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                logging.debug(f"Updated node cost in frontier: {old_cost} -> {state_cost[new_state]}")
+        if frontier:
+            state = frontier.pop()
+        else:
+            state = None
+
+    path = list()
+    s = state
+    while s:
+        path.append(s.copy_data())
+        s = parent_state[s]
+
+    print(f"A* W={sum(len(_) for _ in state._data)}; bloat={(sum(len(_) for _ in state._data)-N)/N*100:.0f}%")
+    #print(f"Initial blocks : {all_list}")
+    print(f"Solution: {state}")
+    return list(reversed(path))            
+
+parent_state = dict()
+state_cost = dict()
+initial_state = State(np.array([]))
+
+for N in [5,10,20,100,500,1000]:
+    start = time.time()
+    GOAL = set(range(N))
+    all_lists = problem(N, seed=42)
+
+    print(f"Problem with N={N}")
+
+    search(
+        all_lists,
+        initial_state = initial_state,
+        goal_test = goal_test,
+        parent_state = parent_state,
+        state_cost = state_cost,
+        priority_function = lambda s: state_cost[s] + h(s),
+        unit_cost = lambda a: len(a),
+        number = N
+        )
+    end = time.time()
+
+    print(f"Done in {end-start} seconds\n\n")
