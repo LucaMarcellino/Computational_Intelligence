5ea5f8143826c74eea8f0b809c01dd38913565b7
Luca Marcellino <s234262@studenti.polito.it>
2022-10-18 23:21:47 +0200

    try

--- Lab1/lab1/__init__.py
+++ Lab1/lab1/__init__.py
@@ -11,7 +11,7 @@ def problem(N, seed=42):
     random.seed(seed)
     return [
         list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
-        for n in range(random.randint(N, N * 5))
+        for n in range(random.randint(N, N * 5))  
     ]
 
 class State:
